ed765d7ee57d35799330fd0c09216bc18e0a583f
VictorSully79 <victorsullivan@hotmail.com>
2022-02-12 04:00:15 -0800

    Lab 18

--- src/cipher.py
+++ src/cipher.py
@@ -1,7 +1,7 @@
 from nltk.corpus import words as nltk
 
 
-def encrypt(message, shift)
+def encrypt(message, shift):
     res = ""
 
     for char in message:
@@ -22,3 +22,45 @@ def encrypt(message, shift)
             res += char
 
     return res
+
+
+def decrypt(message, shift):
+    return encrypt(message, 26-shift)
+
+
+def crack(message):
+    word_list = nltk.words()
+    res = ''
+    max_words_matched = 0
+
+    for shift in range(1,26):
+        decrypted = decrypt(message, shift)
+        words_matched = 0
+        for word in decrypted.split(' '):
+            if word in word_list:
+                words_matched += 1
+        
+        #### Check for 50% Match ####
+        if words_matched > len(message) // 2:
+            return decrypted
+        
+        #### Return the match with most words ####
+        elif words_matched > max_words_matched:
+            max_words_matched = words_matched
+            res = decrypted
+            
+    return res
+
+if __name__ == '__main__':
+
+    shift = 10
+    msg = 'Lets make it funky'
+    print(f'message: {msg}')
+    encrypted = encrypt(msg, shift)
+    print(f'encrypting: {encrypted}')
+    decrypted = decrypt(encrypted, shift)
+    print(f'decrypted: {decrypted}')
+
+    print(f'decrypting with no shift: {encrypted}')
+    cracked = crack(encrypted)
+    print(f'Message Cracked: {cracked}')
